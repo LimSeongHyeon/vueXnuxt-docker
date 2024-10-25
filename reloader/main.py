cb394d2b50f20c067a0acb67820f87ae2eb83b93
LimSeongHyeon <87383201+LimSeongHyeon@users.noreply.github.com>
2024-10-25 11:39:03 +0900

    비동기 배포 및 workdir 설정

--- reloader/main.py
+++ reloader/main.py
@@ -1,5 +1,5 @@
 import docker
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, BackgroundTasks, Request, HTTPException
 from dotenv import load_dotenv
 import os
 import hmac
@@ -13,16 +13,27 @@ REPOSITORY = os.getenv("REPOSITORY")
 lock_file = "/shared/build.lock"
 
 
-def checkSignature(signature, payload):
-    if not signature:
-        raise HTTPException(status_code=403, detail="Missing X-Hub-Signature")
+def commandRun(command, workdir='/usr/src/app/front-deploy'):
+    print(f"command > {command}")
+    result = vue_container.exec_run(command, workdir=workdir)
+    print(result.output.decode())
+    print()
 
-    mac = hmac.new(WEBHOOK_SECRET.encode(), msg=payload, digestmod=hashlib.sha256)
-    expected_signature = "sha256=" + mac.hexdigest()
 
-    if not hmac.compare_digest(expected_signature, signature):
-        raise HTTPException(status_code=403, detail="Invalid X-Hub-Signature")
+def reload_vue_container():
+    try:
+        # lock 설정
+        open(lock_file, 'w').close()
 
+        # Vue 컨테이너 내에서 명령어 실행
+        commandRun("git reset --hard origin/release")
+        commandRun("npm install --legacy-peer-deps --ignore-scripts")
+        commandRun("npm run build")
+        commandRun("pm2 restart front")
+
+    finally:
+        # lock 삭제
+        os.remove(lock_file)
 
 @app.post("/github-webhook")
 async def handle_webhook(request: Request):
@@ -30,8 +41,19 @@ async def handle_webhook(request: Request):
     signature = request.headers.get("X-Hub-Signature-256")
     event = request.headers.get("X-GitHub-Event")
     payload = await request.json()
+    raw_payload = await request.body()
 
-    checkSignature(signature, await request.body())
+    if os.path.exists(lock_file):
+        raise HTTPException(status_code=429, detail="Build in progress")
+
+    if not signature:
+        raise HTTPException(status_code=403, detail="Missing X-Hub-Signature")
+
+    mac = hmac.new(WEBHOOK_SECRET.encode(), msg=raw_payload, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + mac.hexdigest()
+
+    if not hmac.compare_digest(expected_signature, signature):
+        raise HTTPException(status_code=403, detail="Invalid X-Hub-Signature")
     
     if event != "push":
         raise HTTPException(status_code=200, detail="Ignoring event other than 'push'")
@@ -39,25 +61,10 @@ async def handle_webhook(request: Request):
     if payload.get("ref") != "refs/heads/release":
         raise HTTPException(status_code=200, detail="Ignoring branch other than 'release'")
 
-    if os.path.exists(lock_file):
-        raise HTTPException(status_code=429, detail="Build in progress")
+    if payload.get("repository").get("full_name") != REPOSITORY:
+        raise HTTPException(status_code=200, detail=f"Ignoring repository other than '{REPOSITORY}'")
+    
+    background_tasks.add_task(reload_vue_container)
+    return {"status": "Build process started, frontend update will proceed in the background"}
 
-    try:
-        # lock 설정
-        open(lock_file, 'w').close()
-        print(payload.get("repository").get("full_name"))
 
-        # GitHub에서 release 브랜치에 대한 푸시 이벤트인지 확인
-        if payload.get("ref") == "refs/heads/release":
-            # Vue 컨테이너 내에서 명령어 실행
-            vue_container.exec_run("cd /usr/src/app/front-deploy")
-            vue_container.exec_run("git reset --hard origin/release")
-            vue_container.exec_run("npm install --legacy-peer-deps --ignore-scripts")
-            vue_container.exec_run("npm run build")
-            vue_container.exec_run("pm2 restart front")
-
-        return {"status": "Frontend updated"}
-
-    finally:
-        # lock 삭제
-        os.remove(lock_file)
